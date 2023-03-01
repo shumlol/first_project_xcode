cc29ffec1e586f98a06584f76871386f4d97d292
Vadim Akhma <shummmmmm@gmail.com>
2023-03-01 17:48:44 +0600

    Initial Commit

---
+++ first_project/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  first_project
+//
+//  Created by Вадим Ахматханов on 01.03.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ first_project/first_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  first_projectApp.swift
+//  first_project
+//
+//  Created by Вадим Ахматханов on 01.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct first_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
